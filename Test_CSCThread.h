0bdc584e714a6c26a8b2cfbef879d85218f9dffa
scpark98 <scpark98@gmail.com>
2026-02-06 11:10:16 +0900

    초기 커밋.

---
+++ Test_CSCThread.h
@@ -0,0 +1,36 @@
+﻿
+// Test_CSCThread.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#ifndef _AFX_NO_MFC_CONTROLS_IN_DIALOGS  // 이 줄을 추가
+#define _AFX_NO_MFC_CONTROLS_IN_DIALOGS  // 경고 메시지 대응
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CTestCSCThreadApp:
+// 이 클래스의 구현에 대해서는 Test_CSCThread.cpp을(를) 참조하세요.
+//
+
+class CTestCSCThreadApp : public CWinApp
+{
+public:
+	CTestCSCThreadApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CTestCSCThreadApp theApp;
---
+++ Test_CSCThreadDlg.h
@@ -0,0 +1,53 @@
+﻿
+// Test_CSCThreadDlg.h: 헤더 파일
+//
+
+#pragma once
+
+#include "SCThread.h"
+#include "Common/CEdit/RichEditCtrlEx/RichEditCtrlEx.h"
+
+// CTestCSCThreadDlg 대화 상자
+class CTestCSCThreadDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CTestCSCThreadDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+	CSCThread	m_thread[3];
+	void		thread_function(int index, CSCThread& th);
+
+	int			m_index = -1;
+	void		update_button_state();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_TEST_CSCTHREAD_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedCancel();
+	afx_msg void OnWindowPosChanged(WINDOWPOS* lpwndpos);
+	CRichEditCtrlEx m_rich;
+	afx_msg void OnBnClickedRadioThread0();
+	afx_msg void OnBnClickedRadioThread1();
+	afx_msg void OnBnClickedRadioThread2();
+	afx_msg void OnBnClickedBtnStart();
+	afx_msg void OnBnClickedBtnPauseResume();
+	afx_msg void OnBnClickedBtnStop();
+};
